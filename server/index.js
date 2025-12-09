a13d2f4894e8d3725cd73181d16e0fa4ea032c3e
Sumit Sharma <sumitsharma6389@gmail.com>
2025-12-09 18:12:43 +0530

    Fix CORS for render frontend

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const SECRET = "secret";
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ces-ctf-1.onrender.com",
   credentials: true
 }));
 app.use(cookieParser());
