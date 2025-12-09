439eef2b77cc158ef6118dda503d60120381f200
Sumit Sharma <sumitsharma6389@gmail.com>
2025-12-09 17:41:45 +0530

    Clean commit without node_modules

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  const login = async () => {
+    const res = await fetch("http://localhost:3000/login", {
+      credentials: "include"
+    });
+    const text = await res.text();
+    setMessage(text);
+  };
+
+  const admin = async () => {
+    const res = await fetch("http://localhost:3000/admin", {
+      credentials: "include"
+    });
+    const text = await res.text();
+    setMessage(text);
+  };
+
+  return (
+    <div style={{ fontFamily: "Arial", padding: "40px" }}>
+      <h1>JWT Breaker CTF</h1>
+
+      <button onClick={login}>Login</button>
+      <button onClick={admin} style={{ marginLeft: "10px" }}>
+        Admin
+      </button>
+
+      <pre style={{ background: "#eee", padding: "20px", marginTop: "20px" }}>
+        {message}
+      </pre>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+const SECRET = "secret";
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+app.use(cookieParser());
+
+
+app.get("/login", (req, res) => {
+  const token = jwt.sign(
+    { username: "user", role: "user" },
+    SECRET
+  );
+
+  res.cookie("token", token, {
+    httpOnly: false,
+    sameSite: "lax"
+  });
+
+  res.send("Logged in as user");
+});
+
+
+app.get("/admin", (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).send("No token found");
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+
+    if (decoded.role === "admin") {
+      return res.send("FLAG:ZmxhZ0NFU3tIZXJlX2lzX3lvdXJfZmxhZ30K");
+    } else {
+      return res.send("Access Denied");
+    }
+  } catch {
+    return res.status(401).send("Invalid token");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Backend running on http://localhost:3000");
+});
